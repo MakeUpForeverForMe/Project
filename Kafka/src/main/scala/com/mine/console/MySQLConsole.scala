d1b527a6fbb2e43f673718dcbfeb35489bdbc603
魏喜明 <664651151@qq.com>
2020-04-10 21:08:41 +0800

    2020-04-10 提交 学习 Kafka 获取 MySQL 数据，整套流程如何编写

--- Kafka/src/main/scala/com/mine/console/MySQLConsole.scala
+++ Kafka/src/main/scala/com/mine/console/MySQLConsole.scala
@@ -15,7 +15,13 @@ sealed class MySQLConsole {
 
     def executeSelect(connection: Connection, sql: String, args: String*) = {
         val ppStatement = if (args.isEmpty) connection.prepareStatement(sql) else connection.prepareStatement(sql, args.toArray)
-        ppStatement.executeQuery()
+        val resultSet = ppStatement.executeQuery()
+        val metaData = resultSet.getMetaData
+        val columnList = (for (i <- 1 to metaData.getColumnCount) yield metaData.getColumnLabel(i)).toList
+        columnList.reduceLeft(_ + "\t" + _)
+        //        while (resultSet.next()) {
+        //            println(resultSet.getObject(metaData.getColumnLabel(i)))
+        //        }
     }
 
     def close(connection: Connection, statement: Statement): Unit = {
@@ -30,7 +36,8 @@ sealed class MySQLConsole {
 
 object ConsoleTest {
     def main(args: Array[String]): Unit = {
-        val connection = MySQLConsole.connection(PropertyPrepare.MYSQL_DRIVER, PropertyPrepare.MYSQL_URL)
-
+        // val connection = MySQLConsole.connection(PropertyPrepare.MYSQL_DRIVER, PropertyPrepare.MYSQL_URL)
+        val connection = MySQLConsole.connection(PropertyPrepare.MYSQL_DRIVER, "jdbc:mysql://localhost:3306/dm_cf?user=root&password=password&characterEncoding=utf8&useSSL=false")
+        MySQLConsole.executeSelect(connection, "select cTime,sex,city from client_info")
     }
 }
--- Kafka/src/test/scala/com/mine/MySQLConsoleTest.scala
+++ Kafka/src/test/scala/com/mine/MySQLConsoleTest.scala
@@ -1,5 +1,13 @@
 package com.mine
 
+import org.junit.Test
+
 class MySQLConsoleTest {
 
+    @Test
+    def aaTest(): Unit = {
+        Array("CA:California", "WA:Washington", "OR:Oregon").
+                map(s => s.split(":")).
+                map { case Array(f1, f2) => (f1, f2) }.foreach(println)
+    }
 }
