881223dc85a1c65f8f5159ac2fe8dfcce85365ae
魏喜明 <ximing.wei@weshareholdings.com>
2021-04-21 09:46:52 +0800

    添加Hive元数据信息生成Excel脚本

--- _python/python_xlwings/HiveMetastoreToExcel.py
+++ _python/python_xlwings/HiveMetastoreToExcel.py
@@ -10,7 +10,7 @@ import logging
 import pymysql
 import xlwings
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s[line:%(lineno)d] - %(levelname)s: %(message)s')
 logger = logging.getLogger("MySqlUtil")
 
 
@@ -23,6 +23,10 @@ def tb_type_enum(table_type):
         return 'view'
     else:
         return 'table'
+
+
+def rgb2hex(rgb: tuple):
+    return int('%2x%2x%02x' % (rgb[2], rgb[1], rgb[0]), 16)
 
 
 class MySqlUtil(object):
@@ -309,28 +313,172 @@ app.screen_updating = False
 
 sheet_names = [sheet.name for sheet in excel.sheets]
 
-for dt_val in dt_map.values():
-    tb_count = len(dt_val.keys()) - 2
+try:
+    for dt_val in dt_map.values():
+        tb_count = len(dt_val.keys()) - 2
 
-    if dt_val[db_name] in sheet_names:
-        excel.sheets[dt_val[db_name]].delete()
-    excel.sheets.add(dt_val[db_name], after=after)
+        if dt_val[db_name] in sheet_names:
+            excel.sheets[dt_val[db_name]].delete()
+            logger.info(f'删除 Sheet {dt_val[db_name]} 成功！')
+        excel.sheets.add(dt_val[db_name], after=after)
+        logger.info(f'添加 Sheet {dt_val[db_name]} 成功！')
 
-    sheet = excel.sheets[dt_val[db_name]]
+        sheet = excel.sheets[dt_val[db_name]]
+        sheet_name = sheet.name
+        logger.info(f'获取到 Sheet {sheet_name}！')
 
-    sheet['A1'].value = f'{dt_val[db_name]} {dt_val[db_comm]}'
+        row_height = 27.75
+        sheet['A1'].row_height = row_height  # 设置第1行 行高
+        logger.info(f'设置 {sheet_name} A1 行高为 {row_height}！')
+        col_width = (10.75, 61.38, 15.5, 82.25, 8.25, 8.25, 10.75, 10.75, 32)
+        for table_index, width in enumerate(col_width):
+            sheet[f'{chr(97 + table_index)}1'].column_width = width  # 设置 列宽
+            logger.info(f'设置 {sheet_name} {chr(97 + table_index)} 列宽为 {width}！')
 
-    """ 合并 单元格 """
-    sheet['A1:I1'].api.merge()
+        """ 标题 设置 """
+        sheet['A1'].value = f'{dt_val[db_name]}    {dt_val[db_comm]}'
+        logger.info(f'设置 {sheet_name} A1 值为 {sheet["A1"].value}！')
+        """ 合并 单元格 """
+        a1i1 = sheet['A1:I1']
+        a1i1.api.merge()
+        logger.info(f'设置 {sheet_name} A1:I1 合并！')
+        """ 设置 字体 """
+        font_color = (5, 99, 193)
+        a1i1.api.Font.Color = rgb2hex(font_color)  # 设置字体颜色
+        logger.info(f'设置 {sheet_name} A1 字体颜色为 {font_color}！')
+        font_size = 22
+        a1i1.api.Font.Size = font_size  # 设置字体大小
+        logger.info(f'设置 {sheet_name} A1 字体大小为 {font_size}！')
+        is_bold = True
+        a1i1.api.Font.Bold = is_bold  # 设置为粗体
+        logger.info(f'设置 {sheet_name} A1 字体粗体 {is_bold}！')
+        """ 设置 单元格边框 """
+        line_style = 1
+        a1i1.api.Borders.LineStyle = line_style  # 全框实线
+        logger.info(f'设置 {sheet_name} A1 单元格边框线型 {line_style}！')
+        line_weight = 3
+        a1i1.api.Borders.Weight = line_weight  # 边框宽度
+        logger.info(f'设置 {sheet_name} A1 单元格边框宽度 {line_weight}！')
+        line_color = (255, 192, 0)
+        a1i1.api.Borders.Color = rgb2hex(line_color)  # 边框颜色
+        logger.info(f'设置 {sheet_name} A1 单元格边框颜色 {line_color}！')
 
-    # if dt_val[db_name] == db_name:
-    # elif dt_key == db_comm:
-    #     pass
-    # elif type(dt_key) is int:
-    #     for tb_key, tb_val in dt_val.items():
-    #         print(dt_key, tb_key, tb_val)
+        table_name_index = 3
+        sheet[f'B{table_name_index}'].value = '表名'
+        sheet[f'C{table_name_index}'].value = '类型'
+        sheet[f'D{table_name_index}'].value = '表注释'
+        logger.info(f'设置 {sheet_name} 页面头部 表头信息！')
 
-""" 关闭 Excel 文档 """
-excel.save()
-excel.close()
-app.quit()
+        b3d3 = sheet['B3:D3']
+        b3d3.api.HorizontalAlignment = -4108
+        logger.info(f'设置 {sheet_name} 页面头部 字段居中！')
+
+        for table_index in range(tb_count):
+            tb_map = dict(dt_val[table_index])
+            logger.info(f'获取 {sheet_name} 库中的表 {tb_map[tb_name]}！')
+
+            table_name_index += 1
+            sheet[f'B{table_name_index}'].value = tb_map[tb_name]
+            sheet[f'C{table_name_index}'].value = tb_map[tb_type]
+            sheet[f'D{table_name_index}'].value = tb_map[tb_comm]
+            logger.info(f'设置 {sheet_name} 页面头部 表信息 {tb_map[tb_name]} {tb_map[tb_type]} {tb_map[tb_comm]}！')
+
+            sheet[f'{table_name_index + 1}:{table_name_index + 1}'].api.Insert()
+            logger.info('在 {sheet_name} 页面头部 表信息 插入新行！')
+
+            """ 取最大行数是为了在最后添加新的表 """
+            """ 最大 行数 """
+            table_col_index_max = sheet.api.UsedRange.Rows.count
+            logger.info(f'获取当前页面 {sheet_name} 的最大行数 {table_col_index_max}！')
+
+            table_col_index = table_col_index_max + 2
+            sheet[f'A{table_col_index}'].value = '名称信息'
+            sheet[f'B{table_col_index}'].value = '英文名称'
+            sheet[f'C{table_col_index}'].value = '类型'
+            sheet[f'D{table_col_index}'].value = '汉语注释'
+            sheet[f'E{table_col_index}'].value = '来源库'
+            sheet[f'F{table_col_index}'].value = '来源表'
+            sheet[f'G{table_col_index}'].value = '来源字段'
+            sheet[f'H{table_col_index}'].value = '计算逻辑'
+            sheet[f'I{table_col_index}'].value = '备注'
+            logger.info(f'设置 {sheet_name} 表信息 表头！')
+
+            ai3 = sheet[f'A{table_col_index}:I{table_col_index}']
+            ai3.api.Font.Size = 14
+            ai3.api.HorizontalAlignment = -4108
+            ai3.api.Borders.LineStyle = 1
+            ai3.api.Borders.Weight = 3
+            ai3.api.Borders(11).Weight = 2
+            logger.info(f'设置 {sheet_name} 表信息 表头 边框、字体大小、单元格居中！')
+
+            table_col_index += 1
+            sheet[f'A{table_col_index}'].value = '表名信息'
+            sheet[f'B{table_col_index}'].value = tb_map[tb_name]
+            sheet[f'C{table_col_index}'].value = tb_map[tb_type]
+            sheet[f'D{table_col_index}'].value = tb_map[tb_comm]
+            logger.info(f'设置 {sheet_name} 表信息 {tb_map[tb_name]}！')
+
+            part_index_start = table_col_index
+
+            if len(tb_map.keys()) - 3 == 2:
+                logger.info(f'{sheet_name} 中表 {tb_map[tb_name]} 有分区，设置 表信息 的 分区信息！')
+                for col_index, col_val in tb_map[1].items():
+                    table_col_index += 1
+                    sheet[f'A{table_col_index}'].value = '分区信息'
+                    sheet[f'B{table_col_index}'].value = col_val[col_name]
+                    sheet[f'C{table_col_index}'].value = col_val[col_type]
+                    sheet[f'D{table_col_index}'].value = col_val[col_comm]
+                    logger.info(f'设置 {sheet_name} 表信息 {tb_map[tb_name]} 的第 {col_index} 个分区的分区信息 {col_val[col_name]}！')
+                logger.info(f'设置 {sheet_name} 表信息 {tb_map[tb_name]} 的分区信息完成！')
+
+            ai3_partition = sheet[f'A{part_index_start}:I{table_col_index}']
+            ai3_partition.api.Font.Size = 14
+            ai3_partition.api.Borders.LineStyle = 1
+            ai3_partition.api.Borders.Weight = 3
+            ai3_partition.api.Borders(11).Weight = 2
+            ai3_partition.api.Borders(12).Weight = 2
+            logger.info(f'设置 {sheet_name} 表信息 {tb_map[tb_name]} 分区部分的 边框、字体大小、单元格居中！')
+
+            col_index_start = table_col_index + 1
+
+            for col_index, col_val in tb_map[0].items():
+                table_col_index += 1
+                sheet[f'A{table_col_index}'].value = '字段信息'
+                sheet[f'B{table_col_index}'].value = col_val[col_name]
+                sheet[f'C{table_col_index}'].value = col_val[col_type]
+                sheet[f'D{table_col_index}'].value = col_val[col_comm]
+                logger.info(f'设置 {sheet_name} 表信息 {tb_map[tb_name]} 的第 {col_index} 个字段的字段信息 {col_val[col_name]}！')
+            logger.info(f'设置 {sheet_name} 表信息 {tb_map[tb_name]} 的字段信息完成！')
+
+            ai3_col = sheet[f'A{col_index_start}:I{table_col_index}']
+            ai3_col.api.Font.Size = 14
+            ai3_col.api.Borders.LineStyle = 1
+            ai3_col.api.Borders.Weight = 3
+            ai3_col.api.Borders(11).Weight = 2
+            ai3_col.api.Borders(12).Weight = 2
+            logger.info(f'设置 {sheet_name} 表信息 {tb_map[tb_name]} 字段部分的 边框、字体大小、单元格居中！')
+        logger.info(f'设置 {sheet_name} 页面的表信息完成！')
+
+        b3d3.api.Borders.LineStyle = 1
+        b3d3.api.Borders.Weight = 3
+        b3d3.api.Borders(11).Weight = 2
+        b3d3.api.Font.Size = 14
+        logger.info(f'设置 {sheet_name} 页面头部 表头 部分的 边框、字体大小、单元格居中！')
+
+        b4di = sheet[f'B4:D{table_name_index}']
+        b4di.api.Borders.LineStyle = 1
+        b4di.api.Borders.Weight = 3
+        b4di.api.Borders(11).Weight = 2
+        b4di.api.Borders(12).Weight = 2
+        b4di.api.Font.Size = 14
+        logger.info(f'设置 {sheet_name} 页面头部 表信息 部分的 边框、字体大小、单元格居中！')
+
+        logger.info(f'设置 {sheet_name} 页面完成！')
+finally:
+    """ 关闭 Excel 文档 """
+    excel.save()
+    logger.info('Excel 保存成功！')
+    excel.close()
+    logger.info('Excel 关闭成功！')
+    app.quit()
+    logger.info('Excel 退出成功！')
